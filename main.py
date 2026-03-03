128c83899a232fbd5b434ed908e4ddd411239e18
Ludmila <ludmila.gaidam.711@cranfield.ac.uk>
2026-03-03 23:09:22 +0000

    Debugging done, csv file fixed, adding, modifying and deleting functions all added or improved

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import random
 
 file = "reservations.csv"
 df = pd.read_csv(file) # read csv file 
+#the 3 lines below are AI generated as I did not understand why all of a sudden all of my integers in the csv had a .0 added at the end of them. Code is fully understood, no modifications. I pasted my code and asked why it did that.
+int_columns = ["id", "num_nights", "num_adults", "num_children", "room_num"]
+for col in int_columns:
+    df[col] = df[col].fillna(0).astype(int)
+df.to_csv(file, index=False)
 
 
 def show_menu(): # function show_menu() => print options
@@ -17,10 +22,9 @@ def show_menu(): # function show_menu() => print options
     print("2. Create a new reservation")
     print("3. Modify reservation")
     print("4. Cancel reservation")
-    print("5. Search for reservation(s)")
-    print("6. Exit")
+    print("5. Exit")
     print("~"*40)
-    return input("Please choose from options 1-6 by typing the corresponding number, e.g 3 = Modify reservation: ")
+    return input("Please choose from options 1-5 by typing the corresponding number, e.g 3 = Modify reservation: ")
 
 def load_reservations(file): # function load_reservation() as a table 
 
@@ -194,12 +198,134 @@ def add_reservation(): # function add_reservation()
 
     comments = input("Comments or special requests: ")
     
-    new_reservation = f"{new_id}, {first_name.title()}, {last_name.title()}, {checkin_date}, {checkout_date}, {num_nights}, {num_adults}, {num_children}, {room_type}, {room_num}, {guest_type}, {comments}"
+    new_reservation = f"\n{int(new_id)}, {first_name.title()}, {last_name.title()}, {checkin_date}, {checkout_date}, {int(num_nights)}, {int(num_adults)}, {int(num_children)}, {room_type}, {int(room_num)}, {guest_type}, {comments}"
 
     with open(file, "a") as f: #add reservation to csv
         f.write(new_reservation)
     
     return new_reservation
+
+def modify_reservation():
+    """lines 212-218 were initially code generated but I initially didn't use it. I wrote my own code which was very similar but didn't have .values and 
+    didn't have global df, I then added those elements in too see what was stopping my code from working. I then asked it to explaint the purpose of both
+    and the generated code didn't have a loop but I wanted the program to keep asking the user for the ID until they input a correct one so added that in"""""
+    global df 
+    
+    search_id = input("Enter the reservation ID of reservation you wish to modify: ")
+    
+    while search_id not in df["id"].astype(str).values:
+            print("Reservation ID not found, please try again")
+            search_id = input("Please enter valid reservation ID: ")
+
+    print(f"Here are the reservation details for ID: {search_id}")    
+    print(df[df["id"].astype(str) == search_id].to_string(index=False))
+
+    print("What would you like to modify?")
+    print("1. First name")
+    print("2. Last name")
+    print("3. Check in date")
+    print("4. Check out date")
+    print("5. Number of nights")
+    print("6. Number of adults")
+    print("7. Number of children")
+    print("8. Room type")
+    print("9. Room number")
+    print("10. Guest type")
+    print("11. Comments")
+
+    mod = input("Enter a number between 1-10: ")
+
+    choice_dict = { #create dictionary so users choice corresponds to column name
+        "1" : "first_name",
+        "2" : "last_name",
+        "3" : "checkin_date",
+        "4" : "checkout_date",
+        "5" : "num_nights",
+        "6" : "num_adults",
+        "7" : "num_children",
+        "8" : "room_type",
+        "9" : "room_num",
+        "10" : "guest_type",
+        "11" : "comments"
+    }
+
+    value = choice_dict[mod] #column name corresponding to user choice
+
+    new = ""
+
+    if value == "first_name" or value == "last_name":
+        new = name_val(input("Enter new: ")) #assign the input to the variable new
+        while not new:
+            new = name_val(input("Invalid, try again"))
+
+    elif value == "checkin_date" or value == "checkout_date":
+        new = date_val(input("Enter new date in DD/MM/YYYY format: "))
+        while not new:
+            new = date_val(input("Invalid, try again"))
+
+    elif value == "num_nights":
+        new = input("Enter number of nights: ")
+
+    elif value == "num_adults":
+        new = adults_val(input("Enter number of adults: "))
+        while not new:
+            new = adults_val(input("Invalid, try again"))
+
+    elif value == "num_children":
+        new = child_val(input("Enter number of children: "))
+        while not new:
+            new = child_val(input("Invalid, try again"))
+
+    elif value == "room_type":
+        new = room_type_val(input("Enter new room type: "))
+        while not new:
+            new = room_type_val(input("Invalid, try again"))
+    
+    elif value == "room_num":
+        new = input("Enter new room number: ")
+
+    elif value == "guest_type":
+        new = guest_type_val(input("Enter guest type: "))
+        while not new:
+            new = guest_type_val(input("Invalid, try again"))
+    
+    elif value == "comments":
+        new = input("Enter new comments: ") 
+    
+    if value in int_columns:
+        df.loc[df["id"].astype(str) == search_id, value] = int(new) # for integer values converts user input to integer
+    else:
+        df.loc[df["id"].astype(str) == search_id, value] = new # otherwise just leaves it as a string
+    
+    for col in int_columns:
+        df[col] = df[col].astype(int)
+
+    df.to_csv(file, index=False)
+    print(f"Reservation {search_id} successfuly updated!")
+
+def delete_reservation():
+    global df
+
+    search_id = input("Enter the reservation ID of reservation you wish to cancel: ")
+    while search_id not in df["id"].astype(str).values:
+            print("Reservation ID not found, please try again")
+            search_id = input("Please enter valid reservation ID: ")
+    
+    print("Please check the reservation details:")
+    print(df[df["id"].astype(str) == search_id].to_string(index=False))
+
+    yes_no = input("Are you sure you want to cancel this reservation?")
+
+    if yes_no.lower() == "yes":
+
+        index_delete = df[df["id"].astype(str) == search_id].index #locate row for pandas to drop
+        df=df.drop(index_delete)
+        df.to_csv(file, index=False)
+        print("Reservation cancelled")
+
+    else:
+        print("Reservation not cancelled")
+
 
 # menu function if option == 1 add, else if option == 2 view ... 3 modify, 4 delete, 5 search, 6 break
 def options():
@@ -216,18 +342,15 @@ def options():
             input("Press enter to continue")
 
         elif choice == "3":
-            print("MODIFY FUNCTION NOT YET CREATED")
+            print("Modifying reservation: ")
+            modify_reservation()
             input("Press enter to continue")
 
         elif choice == "4":
-            print("DELETE FUNCTION NOT YET CREATED")
+            delete_reservation()
             input("Press enter to continue")
 
         elif choice == "5":
-            print("SEARCH FUNCTION NOT YET CREATED")
-            input("Press enter to continue")
-
-        elif choice == "6":
             print("Bye, have a great day!")
             break
 
@@ -238,8 +361,6 @@ if __name__ == "__main__":
    options()
 
 
-
-# function modify_reservation()
 # function delete_reservation()
-# function search()
+
 
